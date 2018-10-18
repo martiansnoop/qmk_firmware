2515abd72684090996b5c5aa7fedfdc15d3bea35
Emily Wenberg <martiansnoop@gmail.com>
2018-10-17 20:03:46 -0700

    Revert "Experiment with having alternate modifier keys on the home row"
    This reverts commit 637dd0d2aca86002805e11b1ffe63209afec3b3a.

--- keyboards/planck/keymaps/martiansnoop/keymap.c
+++ keyboards/planck/keymaps/martiansnoop/keymap.c
@@ -42,11 +42,6 @@ enum planck_keycodes {
 #define SHIFT_ENTER MT(MOD_RSFT, KC_ENT)
 #define NAV_TAB LT(_NAV, KC_TAB)
 
-// Home row modifier keys
-#define J_CTL  MT(MOD_LCTL, KC_J)
-#define K_LGUI MT(MOD_LGUI, KC_K)
-#define L_LALT MT(MOD_LALT, KC_L)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -62,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    NAV_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    J_CTL,   K_LGUI,  L_LALT,  KC_SCLN, KC_QUOT,
+    NAV_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER ,
     KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
 ),
