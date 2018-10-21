caf0ce692239a0d1ff1e931f953ceb8198469143
Emily Wenberg <martiansnoop@gmail.com>
2018-10-21 12:34:29 -0700

    Add a basic mouse layer
    Oftentimes when there isn't a lot of room, I like putting my keyboard
    over my laptop's trackpad, so I want to try this.

--- keyboards/planck/keymaps/martiansnoop/config.h
+++ keyboards/planck/keymaps/martiansnoop/config.h
@@ -44,4 +44,11 @@
 #define NO_DEBUG
 #define NO_PRINT
 
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 #endif
+
--- keyboards/planck/keymaps/martiansnoop/keymap.c
+++ keyboards/planck/keymaps/martiansnoop/keymap.c
@@ -26,6 +26,7 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _NAV,
+  _MOUSE,
   _PLOVER,
   _ADJUST
 };
@@ -39,6 +40,7 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define NAV   MO(_NAV)
+#define MOUSE MO(_MOUSE)
 #define SHIFT_ENTER MT(MOD_RSFT, KC_ENT)
 #define NAV_TAB LT(_NAV, KC_TAB)
 
@@ -63,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     NAV_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER ,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
+    KC_LCTL, KC_LALT, KC_LGUI, MOUSE, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
 ),
 
 /* Lower
@@ -110,6 +112,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, KC_END , _______, _______, _______ , _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______
 ),
+
+/* Mouse
+ */
+[_MOUSE] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R , _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______
+   i ),
+
 /* Plover layer (http://opensteno.org)
  * ,-----------------------------------------------------------------------------------.
  * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
