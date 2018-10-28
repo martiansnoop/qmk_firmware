44b4720d38304c03f900ccbba7ad7a45893f2866
Emily Wenberg <martiansnoop@gmail.com>
2018-10-28 13:53:06 -0700

    Add a layer specifically for the F keys

--- keyboards/planck/keymaps/martiansnoop/keymap.c
+++ keyboards/planck/keymaps/martiansnoop/keymap.c
@@ -27,6 +27,7 @@ enum planck_layers {
   _RAISE,
   _NAV,
   _MOUSE,
+  _FKEYS,
   _PLOVER,
   _ADJUST
 };
@@ -41,6 +42,7 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 #define NAV   MO(_NAV)
 #define MOUSE MO(_MOUSE)
+#define FKEYS MO(_FKEYS)
 #define SHIFT_ENTER MT(MOD_RSFT, KC_ENT)
 #define NAV_TAB LT(_NAV, KC_TAB)
 
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,   KC_E,  KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     NAV_TAB, KC_A,    KC_S,   KC_D,  KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER ,
-    KC_LCTL, KC_LALT, KC_LGUI,MOUSE, LOWER,   KC_SPC, KC_SPC,  RAISE,   NAV,     KC_DOWN, KC_UP  , KC_RIGHT
+    KC_LCTL, KC_LALT, KC_LGUI,MOUSE, LOWER,   KC_SPC, KC_SPC,  RAISE,   NAV,     KC_DOWN, KC_UP  , FKEYS 
 ),
 
 /* Lower
@@ -122,6 +124,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______
    ),
 
+/* Function keys 
+ */
+[_FKEYS] = LAYOUT_planck_grid(
+    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+   ),
+
 /* Plover layer (http://opensteno.org)
  * ,-----------------------------------------------------------------------------------.
  * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
@@ -143,19 +154,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * | Wake | Reset| Debug|      |      |      |      |      |      |      |      |      |
+ * | Wake | Reset| Debug|      |      |      |      |      |      |      |Mu tog|Qwerty|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Sleep|      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |Plover|      |
+ * | Sleep|      |      |      |      |AGnorm|AGswap|      |      |      |Au tog|Plover|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|      |      |      |      |      |      |      |
+ * |      |Voice-|Voice+|      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    KC_WAKE, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_SLEP, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, PLOVER,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  _______, _______, _______, _______, _______, _______, _______,
+    KC_WAKE, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, MU_TOG,  QWERTY,
+    KC_SLEP, _______, MU_MOD,  _______, _______, AG_NORM, AG_SWAP, _______, _______, _______, AU_TOG,  PLOVER,
+    _______, MUV_DE,  MUV_IN,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
