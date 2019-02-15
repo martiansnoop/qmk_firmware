740649753e87bc1a724fb91260cb9938868dd954
Emily Wenberg <martiansnoop@gmail.com>
2019-02-14 21:47:50 -0800

    Make preonic top row closer to the second-to-top row

--- keyboards/preonic/keymaps/martiansnoop/keymap.c
+++ keyboards/preonic/keymaps/martiansnoop/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   NAV_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER,  \
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Nav
  */
 [_NAV] = LAYOUT_preonic_grid( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______, \
-    _______, _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______, KC_PGUP, KC_UP  , KC_PGDN,  _______, _______ , \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, KC_DEL, \
+    _______, _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, _______, KC_PGUP, KC_UP  , KC_PGDN,  _______, KC_DEL , \
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, \
     _______, _______, _______, _______, _______, _______, KC_END , _______, _______, _______ , _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______  \
