810263b1f5f77ed8085c1f174086dccc80f34a90
Emily Wenberg <martiansnoop@gmail.com>
2019-07-07 22:09:20 -0700

    Make tmux key double as ctrl when held
    Trying this out as an alternative to using autokey to move keyboard
    shortcuts over to the super key.

--- keyboards/planck/keymaps/martiansnoop/keymap.c
+++ keyboards/planck/keymaps/martiansnoop/keymap.c
@@ -47,7 +47,9 @@ enum planck_keycodes {
 // tab navigation
 #define CTL_TAB LCTL(KC_TAB)
 #define CSF_TAB LSFT(LCTL(KC_TAB))
+
 #define TMUX    LCTL(KC_B)
+#define CTL_TMX MT(MOD_LCTL, TMUX)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -63,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,   KC_E,  KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    NAV_TAB, KC_A,    KC_S,   KC_D,  KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,   KC_C,  KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER ,
-    KC_LCTL, KC_LALT, KC_LGUI,TMUX,  LOWER,   KC_SPC, KC_SPC,  RAISE,   NAV,     KC_DOWN, KC_UP  , FKEYS
+    KC_ESC,  KC_Q,    KC_W,   KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    NAV_TAB, KC_A,    KC_S,   KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHIFT_ENTER ,
+    KC_LCTL, KC_LALT, KC_LGUI,CTL_TMX,LOWER,   KC_SPC, KC_SPC,  RAISE,   NAV,     KC_DOWN, KC_UP  , FKEYS
 ),
 
 /* Lower
